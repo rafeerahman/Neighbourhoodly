a243fdf465cf53958579916982133497cd0956fa
Steven Lau <stev.lau@mail.utoronto.ca>
2022-03-05 23:45:33 -0500

    updated app to route to profile and added blank profile page

---
+++ neighbourhood.ly/src/components/HeaderBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import '../App.css';
+
+class HeaderBar extends React.Component {
+    // For profile, Will be implemented similar to sidebar, instead using header tab. WIP 
+  }
+  
